c57f8830d81644f0af8d6207590265bdf0cac8b9
Abishek Ayyappan <jettleetrif@gmail.com>
2024-10-05 14:32:43 +0530

    feat: initial commit

---
+++ src/main/java/org/cloudbees/ticketmanagementservice/TicketmanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package org.cloudbees.ticketmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmanagementserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmanagementserviceApplication.class, args);
+	}
+
+}

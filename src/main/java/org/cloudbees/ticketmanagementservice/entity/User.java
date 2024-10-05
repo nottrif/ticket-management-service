950f45b5645fd5b1023bba40230b73e2a728a733
Abishek Ayyappan <jettleetrif@gmail.com>
2024-10-05 22:18:03 +0530

    feat: add domain entity models

---
+++ src/main/java/org/cloudbees/ticketmanagementservice/entity/User.java
@@ -0,0 +1,21 @@
+package org.cloudbees.ticketmanagementservice.entity;
+
+import lombok.Getter;
+
+@Getter
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+    }
+}
